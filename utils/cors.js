c03168e96a700d0d4610f43707beab3ceb80ffe2
pazguille <guille87paz@gmail.com>
2024-02-05 20:15:56 -0300

    Update CORS value.

--- utils/cors.js
+++ utils/cors.js
@@ -1,6 +1,6 @@
 export default {
   'Access-Control-Allow-Methods': 'GET',
   'Access-Control-Allow-Origin': 'https://xstoregames.com',
-  'Access-Control-Allow-Origin': 'https://dev.xstoregames.com',
+  'Access-Control-Allow-Origin': 'https://dev.xstoregames.com:3030',
   'Access-Control-Allow-Credential': 'true'
 };
